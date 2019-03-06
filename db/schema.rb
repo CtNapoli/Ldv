1d088a5fd2a111f87b6704cc03d259cb8284d1d7
Alfonso <criscialfonso92@gmail.com>
2019-03-06 11:16:00 +0100

    Add description to area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_194812) do
+ActiveRecord::Schema.define(version: 2019_03_06_093341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_03_04_194812) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
     t.index ["name"], name: "index_areas_on_name"
   end
 
