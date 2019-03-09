368ff118bac5a03777076279bca24f873985bd3e
Alfonso <criscialfonso92@gmail.com>
2019-03-09 18:42:02 +0100

    Add search form apartments

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -1,5 +1,14 @@
 class ApartmentsController < ApplicationController
     def index
-        @apartments = Apartment.all
+        if params[:where] || params[:guests]
+            @apartments = Apartment.where(area_id: params[:where].to_i).where('capacity >= ?',  params[:guests].to_i)
+        else
+            @apartments = Apartment.all
+        end
+
+        respond_to do |format|
+            format.html
+            format.js
+        end
     end
 end
