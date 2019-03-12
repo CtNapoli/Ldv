1d14c6625596de0fec8c55341b1a78ac2e015693
Alfonso <criscialfonso92@gmail.com>
2019-03-12 08:15:01 +0100

    Add name column to service

--- db/migrate/20190312070904_create_services.rb
+++ db/migrate/20190312070904_create_services.rb
@@ -1,7 +1,7 @@
 class CreateServices < ActiveRecord::Migration[5.2]
   def change
     create_table :services do |t|
-
+      t.string :name, index: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_06_195925) do
+ActiveRecord::Schema.define(version: 2019_03_12_070904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,13 @@ ActiveRecord::Schema.define(version: 2019_03_06_195925) do
     t.string "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_services_on_name"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
