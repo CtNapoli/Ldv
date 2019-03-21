f39540b2f2a2e7ed4de72bce6e98b8037441f8b8
Alfonso <criscialfonso92@gmail.com>
2019-03-21 20:13:18 +0100

    Index backend apartments

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
     get     'login'   => 'sessions#new'
     post    'login'   => 'sessions#create'
     delete  'logout'  => 'sessions#destroy'
+
+    get     'appartamenti' =>  'apartments#index', as: 'apartments'
   end
 end
