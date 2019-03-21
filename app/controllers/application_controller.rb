7a0c327d0629c0b32ffcc30a535e39eb81730c9e
Alfonso <criscialfonso92@gmail.com>
2019-03-21 20:12:49 +0100

    Add helpers for login/logout session admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+    def current_admin
+        @admin ||= Admin.find_by_id(session[:admin_id])
+    end
+    helper_method :current_admin
+    
+    def admin_logged_in?
+        current_admin.is_a? Admin
+    end
+    
+    def authenticate_admin!
+        redirect_to backend_login_path unless admin_logged_in?
+    end
 end
