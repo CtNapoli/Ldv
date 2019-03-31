f91dcd818d1df849a3a3a702661fe82e0c78dd68
Alfonso <criscialfonso92@gmail.com>
2019-03-31 17:38:02 +0200

    Fix all apartment path in session controller

--- app/controllers/backend/sessions_controller.rb
+++ app/controllers/backend/sessions_controller.rb
@@ -9,7 +9,7 @@ class Backend::SessionsController < ApplicationController
         admin = Admin.find_by(username: params[:username])
         if admin && admin.authenticate(params[:password])
             session[:admin_id] = admin.id
-            redirect_to backend_apartments_path
+            redirect_to backend_all_apartments_path
         else
             flash.now.alert = 'Nome utente o password errati'
             render :new
