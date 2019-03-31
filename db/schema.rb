b19e893a92bdee179c23a2a3c42333f8e4a9516c
Alfonso <criscialfonso92@gmail.com>
2019-03-31 18:17:32 +0200

    Add published column to apartment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_18_193511) do
+ActiveRecord::Schema.define(version: 2019_03_31_160904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2019_03_18_193511) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "area_id"
+    t.boolean "published", default: true
     t.index ["area_id"], name: "index_apartments_on_area_id"
   end
 
