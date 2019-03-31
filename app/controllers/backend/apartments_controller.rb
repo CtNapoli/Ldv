bf26e786c528300fdb213b5f2b849832272562f4
Alfonso <criscialfonso92@gmail.com>
2019-03-31 17:36:45 +0200

    Refactor create update new apartment

--- app/controllers/backend/apartments_controller.rb
+++ app/controllers/backend/apartments_controller.rb
@@ -1,17 +1,23 @@
 class Backend::ApartmentsController < BackendController
     before_action :authenticate_admin!
-    before_action :load_apartment, only: [:edit, :update, :destroy]
+    before_action :load_apartment, only: [:edit, :update, :destroy, :remove_main_photo, :remove_photo]
     
     def index
         @apartments = Apartment.all
     end
 
-    def create
-    
+    def new
+        @apartment = Apartment.new
     end
 
-    def new
-        
+    def create
+        @apartment = Apartment.new(apartment_params)
+        if @apartment.save
+            flash.notice = 'Appartamento creato con successo'
+            redirect_to backend_all_apartments_path
+        else
+            render :new
+        end
     end
 
     def edit
@@ -19,15 +25,34 @@ class Backend::ApartmentsController < BackendController
     end
 
     def update
-    
+        if @apartment.update(apartment_params)
+            flash.notice = 'Appartamento modificato con successo'
+            redirect_to backend_edit_apartment_path(@apartment)
+        else
+            render :edit
+        end
     end
 
     def destroy
         
     end
 
+    def remove_main_photo
+        @id = @apartment.main_image.id
+    end
+
+    def remove_photo
+    
+    end
+
 
     def load_apartment
         @apartment = Apartment.find(params[:id])
+    end
+
+    private
+
+    def apartment_params
+        params.require(:apartment).permit(:name, :address, :postal_code, :city, :province, :lat, :lng, :capacity, :square_meters, :rooms, :bedrooms, :toilette, :area_id, :main_image, :images => [])
     end
 end
