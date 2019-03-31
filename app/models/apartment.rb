943e01f8861bf6e0284303b002f27e87eaa7fd90
Alfonso <criscialfonso92@gmail.com>
2019-03-31 17:37:18 +0200

    Add validates to apartment

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -3,4 +3,26 @@ class Apartment < ApplicationRecord
     has_one_attached :main_image
     has_many_attached :images
     has_and_belongs_to_many :services, -> { distinct }
+
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :postal_code, presence: true
+    validates :city, presence: true
+    validates :province, presence: true
+    validates :lat, presence: true
+    validates :lng, presence: true
+    validates :capacity, presence: true
+    validates :square_meters, presence: true
+    validates :rooms, presence: true
+    validates :bedrooms, presence: true
+    validates :toilette, presence: true
+
+    validate :main_image_presence
+
+    private
+
+    def main_image_presence
+        errors.add(:main_image, :blank) unless main_image.attached?
+    end
+
 end
