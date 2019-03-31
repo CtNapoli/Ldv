2f14151a241bd4cd333628f91acb4cf2d7389cc7
Alfonso <criscialfonso92@gmail.com>
2019-03-31 17:37:02 +0200

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,13 @@ Rails.application.routes.draw do
     post    'login'   => 'sessions#create'
     delete  'logout'  => 'sessions#destroy'
 
-    get     'appartamenti'            =>  'apartments#index', as: 'all_apartments'
-    get     'appartamento/:id/edit'   =>  'apartments#edit', as: 'edit_apartment'
-    get     'appartamento/new'        =>  'apartments#new', as: 'new_apartment'
-    post    'appartamenti'            =>  'apartments#create', as: 'create_apartment'
-    delete  'appartamento/:id'        =>  'apartments#destroy', as: 'destroy_apartment'
-    patch   'appartamento/:id'        =>  'apartments#update', as: 'update_apartment'
+    get     'appartamenti'                  =>  'apartments#index', as: 'all_apartments'
+    get     'appartamento/:id/edit'         =>  'apartments#edit', as: 'edit_apartment'
+    get     'appartamento/new'              =>  'apartments#new', as: 'new_apartment'
+    post    'appartamenti'                  =>  'apartments#create', as: 'create_apartment'
+    delete  'appartamento/:id'              =>  'apartments#destroy', as: 'destroy_apartment'
+    patch   'appartamento/:id'              =>  'apartments#update', as: 'update_apartment'
+    delete  'appartamento/:id/edit/remove-main-photo' =>  'apartments#remove_main_photo', as: 'remove_main_photo'
+    delete  'appartamento/:id/edit/remove-photo' =>  'apartments#remove_photo', as: 'remove_photo'
   end
 end
