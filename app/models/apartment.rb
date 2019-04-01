8277a8f02a84ea2d00eb37c7815b43354b9c6df0
Alfonso <criscialfonso92@gmail.com>
2019-04-01 19:14:18 +0200

    Apartment model extend friendly_id & use name as slug

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,4 +1,7 @@
 class Apartment < ApplicationRecord
+    extend FriendlyId
+    friendly_id :name, use: :slugged
+
     belongs_to :area
     has_one_attached :main_image
     has_many_attached :images
