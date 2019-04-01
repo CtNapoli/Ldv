045b20d8461ece70a564bc3754ccaf51d189f370
Alfonso <criscialfonso92@gmail.com>
2019-04-01 20:15:35 +0200

    Add new/create services

--- app/controllers/backend/services_controller.rb
+++ app/controllers/backend/services_controller.rb
@@ -4,4 +4,24 @@ class Backend::ServicesController < BackendController
     def index
         @services = Service.all
     end
+
+    def new
+        @service = Service.new
+    end
+
+    def create
+        @service = Service.new(service_params)
+        if @service.save
+            flash.notice = 'Servizio creato con successo'
+            redirect_to backend_services_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def service_params
+        params.require(:service).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,17 @@ Rails.application.routes.draw do
     post    'login'   => 'sessions#create'
     delete  'logout'  => 'sessions#destroy'
 
-    get     'appartamenti'                  =>  'apartments#index', as: 'all_apartments'
-    get     'appartamento/:id/edit'         =>  'apartments#edit', as: 'edit_apartment'
-    get     'appartamento/new'              =>  'apartments#new', as: 'new_apartment'
-    post    'appartamenti'                  =>  'apartments#create', as: 'create_apartment'
-    delete  'appartamento/:id'              =>  'apartments#destroy', as: 'destroy_apartment'
-    patch   'appartamento/:id'              =>  'apartments#update', as: 'update_apartment'
-    delete  'appartamento/:id/edit/remove-main-photo' =>  'apartments#remove_main_photo', as: 'remove_main_photo'
-    delete  'appartamento/:id/edit/remove-photo' =>  'apartments#remove_photo', as: 'remove_photo'
+    get     'appartamenti'                                          =>  'apartments#index', as: 'all_apartments'
+    get     'appartamento/:id/modifica'                             =>  'apartments#edit', as: 'edit_apartment'
+    get     'appartamento/nuovo'                                    =>  'apartments#new', as: 'new_apartment'
+    post    'appartamenti'                                          =>  'apartments#create', as: 'create_apartment'
+    delete  'appartamento/:id'                                      =>  'apartments#destroy', as: 'destroy_apartment'
+    patch   'appartamento/:id'                                      =>  'apartments#update', as: 'update_apartment'
+    delete  'appartamento/:id/modifica/rimuovi-immagine-principale' =>  'apartments#remove_main_photo', as: 'remove_main_photo'
+    delete  'appartamento/:id/modifica/rimuovi-immagine'            =>  'apartments#remove_photo', as: 'remove_photo'
 
-    get 'servizi'                           =>  'services#index', as: 'services'
+    get     'servizi'                       =>  'services#index', as: 'services'
+    get     'servizi/nuovo'                 =>  'services#new', as: 'new_service'
+    post    'servizi'                       =>  'services#create', as: 'create_service'  
   end
 end
