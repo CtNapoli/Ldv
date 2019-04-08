c0521819606d5c2e02d3a38c573c343be501a65e
Alfonso <criscialfonso92@gmail.com>
2019-04-08 16:27:55 +0200

    Add content to area

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_102129) do
+ActiveRecord::Schema.define(version: 2019_04_08_142626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_04_06_102129) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
+    t.text "content"
     t.index ["name"], name: "index_areas_on_name"
   end
 
