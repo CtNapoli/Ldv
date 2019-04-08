94bcbffb0efa05c549d3f9a90f92ca853b967a66
Alfonso <criscialfonso92@gmail.com>
2019-04-08 17:09:10 +0200

    Add default content value to area

---
+++ db/migrate/20190408150523_add_default_value_to_content_area.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToContentArea < ActiveRecord::Migration[5.2]
+  def change
+    change_column :areas, :content, :text, default: '[]'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_143425) do
+ActiveRecord::Schema.define(version: 2019_04_08_150523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,7 +80,7 @@ ActiveRecord::Schema.define(version: 2019_04_08_143425) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
-    t.text "content"
+    t.text "content", default: "[]"
     t.string "slug"
     t.index ["name"], name: "index_areas_on_name"
     t.index ["slug"], name: "index_areas_on_slug", unique: true
