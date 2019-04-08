b71aee37d05e9c96c1330bae98a9dd2c90fab2da
Alfonso <criscialfonso92@gmail.com>
2019-04-08 17:09:30 +0200

    Add friendly id to area

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,4 +1,7 @@
 class Area < ApplicationRecord
+    extend FriendlyId
+    friendly_id :name, use: :slugged
+    
     has_one_attached :image
     has_many_attached :images
     has_many :apartments
