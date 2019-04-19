24a21ffd674179cabab901251c21acb509e0cc64
Alfonso <criscialfonso92@gmail.com>
2019-04-19 11:30:03 +0200

    Create question table

--- db/migrate/20190419092536_create_questions.rb
+++ db/migrate/20190419092536_create_questions.rb
@@ -1,7 +1,8 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-
+      t.string :request
+      t.text :reply
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_154756) do
+ActiveRecord::Schema.define(version: 2019_04_19_092536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,13 @@ ActiveRecord::Schema.define(version: 2019_04_08_154756) do
     t.date "start"
     t.date "end"
     t.float "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "request"
+    t.text "reply"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
