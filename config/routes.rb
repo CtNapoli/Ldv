9c72107a9fb79a463edece2494e17449878d0aed
Alfonso <criscialfonso92@gmail.com>
2019-04-23 09:53:35 +0200

    Add owner form section

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope "(:locale)", locale: /en|it/  do
     root 'pages#home'
-  
-    post 'send-request' => 'requests#create'
+
     get 'owners'         => 'pages#owners'
+
+    resources :requests, only: [:create]
+
+    resources :owner_requests, only: [:create]
 
     resources :apartments, only: [:index, :show]
     
