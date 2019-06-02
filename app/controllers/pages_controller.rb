d8adcdd1680c585dad4a06c8c56d8d5de21d371a
Alfonso <criscialfonso92@gmail.com>
2019-06-02 17:27:45 +0200

    Fix apartments & area if not exists

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,20 @@
 class PagesController < ApplicationController
+    before_action :load_areas
+    before_action :load_apartments
+
     def home
-        @areas = Area.all
         @featured_apartments = Apartment.where(in_evidence: true).order(created_at: :desc)
-        @apartments = Apartment.all
     end
 
     def owners
         @questions = Question.order(created_at: :desc)
+    end
+
+    def load_areas
+        @areas = Area.all
+    end
+
+    def load_apartments
+        @apartments = Apartment.all
     end
 end
