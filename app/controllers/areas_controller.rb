041f75a3e3e13eb50482a7e0e1401fb9b8886d76
Alfonso <criscialfonso92@gmail.com>
2019-06-02 19:13:29 +0200

    Load areas & apartments into area

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -1,4 +1,7 @@
 class AreasController < ApplicationController
+    before_action :load_areas
+    before_action :load_apartments
+
     def show
         @area = Area.friendly.find(params[:id])
         @paragraphs = JSON.parse @area.content
