2d7520ad72544804e0ef96e19c4def24b5f0e2ed
Alfonso <criscialfonso92@gmail.com>
2019-06-05 20:54:44 +0200

    Add presence true validate on name area

--- app/models/service.rb
+++ app/models/service.rb
@@ -5,5 +5,5 @@ class Service < ApplicationRecord
     
     has_and_belongs_to_many :apartments, -> { distinct }
     
-    validates :name, uniqueness: true
+    validates :name, presence: true, uniqueness: true
 end
