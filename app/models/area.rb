e66b0264fead4c8c1876d193fc3ba8b6731dff21
Alfonso <criscialfonso92@gmail.com>
2019-06-05 20:54:16 +0200

    Add main image area validation

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,7 +1,7 @@
 class Area < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
-    translates :name, :description, :content
+    translates :name, :description, :content, touch: true
     
     has_one_attached :image
     has_many_attached :images
@@ -9,4 +9,14 @@ class Area < ApplicationRecord
 
     validates :name, uniqueness: true, presence: true
     validates :description, presence: true
+
+    validate :main_image_presence
+
+    private
+
+    def main_image_presence
+        errors.add(:image, :blank) unless image.attached?
+    end
+
+    
 end
