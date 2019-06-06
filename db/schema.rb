98da3ecc90c8888a17bfaaee82a9e02dbae9d795
Alfonso <criscialfonso92@gmail.com>
2019-06-06 19:20:13 +0200

    Fix area name nullable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_181925) do
+ActiveRecord::Schema.define(version: 2019_06_06_171333) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,7 +99,7 @@ ActiveRecord::Schema.define(version: 2019_06_05_181925) do
   end
 
   create_table "areas", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "description"
