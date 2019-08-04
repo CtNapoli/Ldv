c6e9def845867aaef2696282bda1ff1d02293a69
Alfonso <criscialfonso92@gmail.com>
2019-08-04 18:56:39 +0200

    Ultimate fixes V2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  scope "(:locale)", locale: /en|\./  do
+  scope "(:locale)", locale: /it|\./  do
     root 'pages#home'
 
     get 'about'          => 'pages#about'
