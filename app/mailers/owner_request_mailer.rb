6a3655f589240ee6976b56fe9df896668419459a
vagrant <vagrant@vagrant.vm>
2020-04-01 11:40:26 +0000

    set to for emails

--- app/mailers/owner_request_mailer.rb
+++ app/mailers/owner_request_mailer.rb
@@ -3,7 +3,7 @@ class OwnerRequestMailer < ApplicationMailer
 
     def owner_request_sent
         @owner_request = params[:owner_request]
-        @to = 'info@ldvhomes.com'
+        @to = ['info@ldvhomes.com', 'vittorioacampora@ldvhomes.com', 'marcoderosa@ldvhomes.com']
         @subject = 'Richiesta partnership'
         mail(to: @to, subject: @subject)
     end
--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -3,7 +3,7 @@ class RequestMailer < ApplicationMailer
 
     def request_sent
         @request = params[:request]
-        @to = 'info@ldvhomes.com'
+        @to = ['info@ldvhomes.com', 'vittorioacampora@ldvhomes.com', 'marcoderosa@ldvhomes.com']
         @subject = 'Richiesta da website'
         mail(to: @to, subject: @subject)
     end
