3e0f6b9be99114ec8befa3ab52b0e345a6a227c1
CtNapoli90 <luigipiccolo.lpl@libero.it>
2021-01-04 16:19:06 +0100

    add my mail when form is submitted

--- app/mailers/booking_request_mailer.rb
+++ app/mailers/booking_request_mailer.rb
@@ -3,7 +3,7 @@ class BookingRequestMailer < ApplicationMailer
 
     def booking_request_sent
         @booking_request = params[:booking_request]
-        @to = ['info@ldvhomes.com', 'vittorioacampora@ldvhomes.com', 'marcoderosa@ldvhomes.com']
+        @to = ['info@ldvhomes.com', 'vittorioacampora@ldvhomes.com', 'marcoderosa@ldvhomes.com', 'luigipiccolo.lpl@gmail.com']
         @subject = 'Richiesta prenotazione'
         mail(to: @to, subject: @subject)
     end
