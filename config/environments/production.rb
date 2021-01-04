a4f2cf37ca2e1e1778012eb0cd2798e30731db21
CtNapoli90 <luigipiccolo.lpl@libero.it>
2021-01-04 10:33:48 +0100

    remove line for test send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,7 +100,7 @@ Rails.application.configure do
     user_name:            'ldvwebsite1@gmail.com',
     password:             'ldvitalia2019',
     authentication:       'plain',
-    enable_starttls_auto: true
+    #enable_starttls_auto: true
   }
 
   config.action_mailer.asset_host = 'http://ldvhomes.com'
