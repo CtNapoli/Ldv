70c8c69124e2d8059971c9265750ee558237368f
CtNapoli90 <luigipiccolo.lpl@libero.it>
2021-03-10 21:26:45 +0100

    remove drop column file

--- db/migrate/20210304205546_remove_column_from_prices.rb
+++
@@ -1,8 +0,0 @@
-class RemoveColumnFromPrices < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :prices, :special_offer_start
-    remove_column :prices, :special_offer_end
-    remove_column :prices, :special_offer_value
-    remove_column :prices, :special_offer
-  end
-end
