23ea6a63b97592505ecbf9bb37f14c5084b6af2d
CtNapoli90 <luigipiccolo.lpl@libero.it>
2024-03-16 12:22:25 +0100

    Change for SEO

--- app/helpers/sitemap_helper.rb
+++ app/helpers/sitemap_helper.rb
@@ -1,11 +1,10 @@
 module SitemapHelper
 
-    def sitemap_cache_key
-      last_apartments_update = Apartment.maximum(:updated_at).try(:utc).try(:to_i)
-  
-      key = "sitemapV2-#{last_apartments_update}"
-      puts "Key is #{key}"
-      key
-    end
-  
+  def sitemap_cache_key
+    last_apartments_update = Apartment.maximum(:updated_at)&.utc&.to_i
+    key = "sitemapV2-#{last_apartments_update}"
+    puts "Key is #{key}" if Rails.env.development? # Stampa la chiave solo in ambiente di sviluppo
+    key
+  end
+    
 end
