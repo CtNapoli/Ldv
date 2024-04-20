c339564a6bc92d543a947e3a90a5dca6dd4a3ca9
CtNapoli90 <luigipiccolo.lpl@libero.it>
2024-04-20 15:22:31 +0200

    fix error sitemap

--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -2,11 +2,6 @@ Sitemap::Generator.instance.load host: "ldvhomes.com" do
     path :root, priority: 1
     path :about, :priority => 0.8
     path :owners, :priority => 0.8
-    # Aggiungi le risorse da includere nella sitemap
-    Apartment.find_each do |apartment|
-        add apartment_path(apartment), priority: 0.6, lastmod: apartment.updated_at
-    end
+    resources :apartments, :priority => 0.8, :change_frequency => "daily"
     
 end
-
-SitemapGenerator::Sitemap.ping_search_engines
