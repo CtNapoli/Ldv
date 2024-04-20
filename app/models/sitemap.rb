f329313338fc5c29523aed8e5b79d5404128b363
CtNapoli90 <luigipiccolo.lpl@libero.it>
2024-04-20 15:01:30 +0200

    remove sitemap.rb

--- app/models/sitemap.rb
+++
@@ -1,7 +0,0 @@
-Sitemap::Generator.instance.load host: "http:\\ldvhomes.com" do
-    path :root, priority: 1
-    path :about, :priority => 0.8
-    path :owners, :priority => 0.8
-    resources :apartments, :priority => 0.8
-    
-  end
