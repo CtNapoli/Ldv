d01c1d82fa74653de15f15a2b95ba26ac5ebb231
CtNapoli90 <luigipiccolo.lpl@libero.it>
2024-05-12 22:05:14 +0200

    update sitemap

--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -1,7 +1,15 @@
 Sitemap::Generator.instance.load host: "ldvhomes.com" do
-    path :root, priority: 1
-    path :about, :priority => 0.8
-    path :owners, :priority => 0.8
-    resources :apartments, :priority => 0.8, :change_frequency => "daily"
+    # path :root, priority: 1
+    # path :about, :priority => 0.8
+    # path :owners, :priority => 0.8
+    # resources :apartments, :priority => 0.8, :change_frequency => "daily"
     
+    path :root, priority: 1, loc: "https://ldvhomes.com/"
+    path :about, priority: 0.8, loc: "https://ldvhomes.com/about"
+    path :owners, priority: 0.8, loc: "https://ldvhomes.com/owners"
+    resources :apartments, priority: 0.8, change_frequency: "daily" do
+        Apartment.all.each do |apartment|
+            path apartment, loc: apartment_url(apartment)
+        end
+    end
 end
