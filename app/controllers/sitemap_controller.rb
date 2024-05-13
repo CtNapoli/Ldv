4a3226f6bbcc62b14ff437ef5f827865bf5e9bab
CtNapoli90 <luigipiccolo.lpl@libero.it>
2024-05-13 21:46:34 +0200

    change loc for sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,7 +2,6 @@ class SitemapController < ApplicationController
 
     def show
         @apartment = Apartment.where(published: true)
-  
         respond_to do |format|
             format.xml
         end
